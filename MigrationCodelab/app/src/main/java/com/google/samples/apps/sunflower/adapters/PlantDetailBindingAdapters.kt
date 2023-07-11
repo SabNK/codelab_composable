d3ca3cb4369ad4ef0480a71aa93e0db087dbbcd8
SabNK <sabnk@optictelecom.ru>
2023-07-11 17:42:05 +0300

    migration completed

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
@@ -16,16 +16,11 @@
 
 package com.google.samples.apps.sunflower.adapters
 
-import android.text.method.LinkMovementMethod
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.text.HtmlCompat
-import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.samples.apps.sunflower.R
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
@@ -45,6 +40,7 @@ fun bindIsGone(view: FloatingActionButton, isGone: Boolean?) {
         view.show()
     }
 }
+/*
 
 @BindingAdapter("renderHtml")
 fun bindRenderHtml(view: TextView, description: String?) {
@@ -61,9 +57,9 @@ fun bindWateringText(textView: TextView, wateringInterval: Int) {
     val resources = textView.context.resources
     val quantityString = resources.getQuantityString(
         R.plurals.watering_needs_suffix,
-        wateringInterval,
         wateringInterval
     )
 
     textView.text = quantityString
 }
+*/
